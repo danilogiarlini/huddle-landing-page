f8e093ce294c5d54eb3a309b9bb644c1a6692f54
Danilo Giarlini <danilogiarlini@gmail.com>
2023-10-15 17:28:43 +0200

    first commit, working on mobile design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logo.svg";
+import illustration from './images/illustration-mockups.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="top-page">
+        <header>
+          <img className='logo' src={logo} alt="logo" />
+        </header>
+        <img className="illustration" src={illustration} alt="illustration"/>
+      </div>
+      <main>
+        <h1>Build The Community Your Fans Will Love</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Huddle te-imagines the way we build communitie. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button>Register</button>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
