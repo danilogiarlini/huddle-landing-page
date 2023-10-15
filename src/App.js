1050b03b4098872c2e765d5f4e293b56d1969fa1
Danilo Giarlini <danilogiarlini@gmail.com>
2023-10-15 19:28:06 +0200

    mobile design finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import "./App.css";
 import logo from "./images/logo.svg";
-import illustration from './images/illustration-mockups.svg'
+import illustration from "./images/illustration-mockups.svg";
 
 function App() {
   return (
     <>
       <div className="top-page">
         <header>
-          <img className='logo' src={logo} alt="logo" />
+          <img className="logo" src={logo} alt="logo" />
         </header>
-        <img className="illustration" src={illustration} alt="illustration"/>
+        <img className="illustration" src={illustration} alt="illustration" />
       </div>
       <main>
         <h1>Build The Community Your Fans Will Love</h1>
         <p>
-          Huddle te-imagines the way we build communitie. You have a voice, but
+          Huddle te-imagines the way we build communities. You have a voice, but
           so does your audience. Create connections with your users as you
           engage in genuine discussion.
         </p>
         <button>Register</button>
       </main>
-      <footer></footer>
+      <footer>
+        <button>
+          <i class="fa-brands fa-facebook-f"></i>
+        </button>
+        <button>
+          <i class="fa-brands fa-twitter"></i>
+        </button>
+        <button>
+          <i class="fa-brands fa-instagram"></i>
+        </button>
+      </footer>
     </>
   );
 }
